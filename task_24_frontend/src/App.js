b59801cda1dfce60cc29c201f09bcc9ade29206e
Omkar Patil <131450067+OmkarPatil987@users.noreply.github.com>
2024-06-26 18:10:55 +0530

    Update App.js

--- task_24_frontend/src/App.js
+++ task_24_frontend/src/App.js
@@ -1,37 +1,4 @@
-// import React from 'react';
-// import { BrowserRouter,Router, Route, Routes } from 'react-router-dom';
-// import Navbar from './components/Navbar';
-// import PersonalInfo from './components/PersonalInfo';
-// import ProfileInfo from './components/ProfileInfo';
-// import AcademicInfo from './components/AcademicInfo';
-// import WorkExperience from './components/WorkExperience';
-// import Home from './components/home';
 
-
-// const App = () => {
-//   return (
-//     <div  className="container mt-3">
-//     <BrowserRouter>
-//       <Navbar />
-//       <div className="container mt-3">
-//         <Routes>
-//         <Route path="/" element={<Home/>} />
-
-//           <Route path="/personal-info" element={<PersonalInfo />} />
-//           <Route path="/profile-info" element={<ProfileInfo />} />
-//           <Route path="/academic-info" element={<AcademicInfo />} />
-//           <Route path="/work-experience" element={<WorkExperience />} />
-//         </Routes>
-//       </div>
-//     </BrowserRouter>
-//     </div>
-//   );
-// };
-
-// export default App;
-
-
-// src/App.js
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar';
